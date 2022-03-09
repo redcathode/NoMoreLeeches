4ef5f88cd810d27ae39436655caedeb879935f33
Max Leon <maxwellleon55243@gmail.com>
2022-03-08 18:58:12 -0600

    CONTINUE

--- win32_window_manager.cxx
+++ win32_window_manager.cxx
@@ -6,12 +6,24 @@ win32_window_manager::win32_window_manager() {
 bool win32_window_manager::update() {
     char wnd_title[256];
     char wnd_class[256];
+    RECT tempRect;
     currentWindow = GetForegroundWindow(); // get handle of currently active window
     GetWindowText(currentWindow, wnd_title, sizeof(wnd_title));
     GetClassName(currentWindow, wnd_class, sizeof(wnd_class));
-    active_window_class.assign(wnd_class);
-    active_window_name.assign(wnd_title);
-    return true;
+    GetWindowRect(currentWindow, &tempRect);
+    std::string tempStr;
+    tempStr.assign(wnd_class);
+    if (tempStr == "") {
+        return false;
+    } else {
+        active_window_class.assign(wnd_class);
+        active_window_name.assign(wnd_title);
+        focusedWindowX = tempRect->left;
+        focusedWindowY = tempRect->top;
+        focusedWindowWidth = tempRect->right - tempRect->left;
+        focusedWindowHeight = tempRect->bottom - tempRect->top;
+        return true;
+    }
 }
 bool win32_window_manager::is_currently_active_window_owned_by_us() {
 //     HWND temp_win = GetForegroundWindow();
